1151325b26f5f7d39f3a041695ee205aa4820ca0
Kévin Holleville <archercelest@gmail.com>
2025-11-18 12:16:23 +0100

    change folder to the root

--- Portfolio/mon-portfolio/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-dark text-white py-6 mt-16 text-center">
-      © 2025 Holleville Kévin. Tous droits réservés.
-    </footer>
-  );
-}
