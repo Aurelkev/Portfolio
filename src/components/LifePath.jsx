f0f5c1afc51fc3aec2f61cdcfb0cf1836dde4bcc
Kévin Holleville <archercelest@gmail.com>
2025-11-25 14:37:33 +0100

    CV done local

--- src/components/LifePath.jsx
+++ src/components/LifePath.jsx
@@ -2,15 +2,15 @@ export default function LifePath() {
   return (
     <div className="flex flex-col items-center justify-center border border-dashed border-gray-400 rounded-xl p-6 bg-white gap-4">
       <a
-        href="/cv"
+        href="./cv"
         className="px-6 py-3 bg-primary-dark text-white font-semibold rounded-lg 
                    hover:bg-primary/80 transition shadow"
       >
         Voir mon CV
       </a>
       <a
-        href="/cv.pdf"
-        download
+        href="./cv/CV.pdf"
+        download="CV_Kevin_Holleville.pdf"
         className="px-6 py-3 bg-black text-white font-semibold rounded-lg 
                    hover:bg-black/80 transition shadow"
       >
